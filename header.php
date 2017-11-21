7a5af022effac044e9cd7cb49c7ddab4aa9a24f3
Aurghyadip <adkundu@gmail.com>
2017-11-22 00:23:11 +0530

    added js file to header

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
 <head>
     <title><?php echo $servername; ?></title>
     <link rel="stylesheet" href="src/css/bootstrap.min.css">
+    <link rel="stylesheet" href="src/css/font-awesome.css">
     <script src="src/js/jquery-3.2.1.min.js"></script>
+    <script src="src/js/bootstrap.min.js"></script>
 </head>
 <body class="bg-light">
